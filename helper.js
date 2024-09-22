b58cc91827bfff02a80002a22e9114ba34858199
VishalSagar7 <155461251+VishalSagar7@users.noreply.github.com>
2024-09-22 12:33:04 +0530

    last commit

--- helper.js
+++ helper.js
@@ -1,2 +1,2 @@
-export const frontEndurl = 'http://localhost:5173';
-// export const frontEndurl = 'https://mybblogapp.netlify.app';
+// export const frontEndurl = 'http://localhost:5173';
+export const frontEndurl = 'https://mybblogapp.netlify.app';
